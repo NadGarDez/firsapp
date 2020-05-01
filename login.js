b55ab7f33709cedfa5257907f627c3f9a416223e
Nad GarDez <iranad@hotmail.com>
2020-05-01 15:55:49 -0400

    solucionado los problemas de deformacion de los componentes al aparecer el teclado en el registro y en el login

--- login.js
+++ login.js
@@ -1,62 +1,110 @@
 import React, { Component } from 'react'; 
-import {StyleSheet,AppRegistry, ScrollView, Image, Text, View, TouchableOpacity,Button,Alert,TextInput,ImageBackground} from 'react-native';
+import {StyleSheet,AppRegistry, ScrollView, Image, Text, View, TouchableOpacity,Button,Alert,TextInput,ImageBackground,Dimensions} from 'react-native';
 
 export default class Ini extends Component{
+
+
+  constructor(props){
+    super();
+
+    this.state={
+
+      'tamanos':{
+        'margenSuperior':0,
+        'cuerpo':0
+      }
+
+    }
+
+    this.calcularTamanos= this.calcularTamanos.bind(this);
+
+    var obj = this.calcularTamanos();
+
+    this.state.tamanos.margenSuperior= obj.margenSuperior;
+    this.state.tamanos.cuerpo = obj.cuerpo;
+
+
+  }
+
+  calcularTamanos(){
+    var width = Dimensions.get('window').width;
+    var height = Dimensions.get('window').height;
+
+    var retorno = {
+
+      margenSuperior:0,
+      cuerpo:0
+
+    };
+
+    retorno.margenSuperior = 4*height;
+    retorno.margenSuperior = retorno.margenSuperior/100;
+    retorno.margenSuperior=Math.trunc(retorno.margenSuperior);
+
+
+    retorno.cuerpo = height-retorno.margenSuperior;
+    
+
+    return retorno;
+
+
+  }
 	
 	render(){
       const image = { uri: "https://static.vecteezy.com/system/resources/previews/000/266/873/non_2x/sky-background-with-clouds-layers-vector.jpg" };
 			return(
 				<View style={{width:'100%',height:'100%'}}>
-					<View style={{width:'100%',height:'4%', backgroundColor:'#053A88'}}>
+					<View style={{width:'100%',height:this.state.tamanos.margenSuperior, backgroundColor:'#053A88'}}>
 						
 					</View>
-					
-					<View style={{width:'100%',height:'96%',backgroundColor:'#1159BF'}}>
-              <ImageBackground source={image} style={{width:'100%',height:'100%'}}>
-						
-                <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
-                  <Text style={{fontSize:35, color:'white'}}>Iniciar Secion</Text>
+					<ScrollView>
+  					<View style={{width:'100%',height:600,backgroundColor:'#1159BF'}}>
+                <ImageBackground source={image} style={{width:'100%',height:'100%'}}>
+  						
+                  <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
+                    <Text style={{fontSize:35, color:'white'}}>Iniciar Secion</Text>
+                    
+                  </View>
+                  <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
                   
-                </View>
-                <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
+                    <TextInput 
+                      style ={estiloInput}
+                      
+                      placeholder ="Nombre de usuario o correo"
+                    />
+                    <TextInput 
+                      style ={estiloInput}
+                      secureTextEntry={true}
+                      placeholder ="Contraceña"
+                    />
+                    
+                    <Button 
+                      
+                      style={{marginBottom:10}}
+                      title="Enviar"
+                      onPress={()=>{
+                        this.props.callback('dashboard',{nombreUser:'Pedro'});
+                      }}
+                      
+                    
+                    />
+                    
+                    <TouchableOpacity style={{marginTop:5 }}>
+                      <View>
+                        <Text style={{fontSize:15, color:'white'}}>¿Olvido su contraceña?</Text>
+                      </View>
+                    
+                    </TouchableOpacity>
+                    
+                  </View>
                 
-                  <TextInput 
-                    style ={estiloInput}
+                  <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
+                    <Text style={{fontSize:20, color:'#053A88'}}>Nad Aplication</Text>
                     
-                    placeholder ="Nombre de usuario o correo"
-                  />
-                  <TextInput 
-                    style ={estiloInput}
-                    secureTextEntry={true}
-                    placeholder ="Contraceña"
-                  />
-                  
-                  <Button 
-                    
-                    style={{marginBottom:10}}
-                    title="Enviar"
-                    onPress={()=>{
-                      this.props.callback('dashboard',{nombreUser:'Pedro'});
-                    }}
-                    
-                  
-                  />
-                  
-                  <TouchableOpacity style={{marginTop:5 }}>
-                    <View>
-                      <Text style={{fontSize:15, color:'white'}}>¿Olvido su contraceña?</Text>
-                    </View>
-                  
-                  </TouchableOpacity>
-                  
-                </View>
-              
-                <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
-                  <Text style={{fontSize:20, color:'#053A88'}}>Nad Aplication</Text>
-                  
-                </View>
-              </ImageBackground>
-					</View>
+                  </View>
+                </ImageBackground>
+  					</View>
+          </ScrollView>
 				</View>
 			
 			);
--- registrar.js
+++ registrar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import {StyleSheet,AppRegistry, ScrollView, Image, Text, View, TouchableOpacity,Button,Alert,TextInput,ImageBackground} from 'react-native';
+import {StyleSheet,AppRegistry, ScrollView, Image, Text, View, TouchableOpacity,Button,Alert,TextInput,ImageBackground,Dimensions} from 'react-native';
 
 import superFetch from "./fechManager.js";
 
@@ -7,11 +7,58 @@ export default class Ini extends Component{
 
   constructor(props){
     super(props);
-    this.state={'correo':{'texto':'','validado':false}, 'contracena':{'uno':'','dos':'','validado':false}}
+    this.state={
+
+      'correo':{'texto':'','validado':false}, 
+      'contracena':{
+        'uno':'',
+        'dos':'',
+        'validado':false
+      },
+      'tamanos':{
+        'margenSuperior':0,
+        'cuerpo':0
+      },
+      'hols':21
+
+    }
     this.validarCorreo= this.validarCorreo.bind(this);
     this.compararContracenas= this.compararContracenas.bind(this);
     this.enviar= this.enviar.bind(this);
     this.isUnique= this.isUnique.bind(this);
+    this.calcularTamanos= this.calcularTamanos.bind(this);
+    
+    var obj = this.calcularTamanos();
+
+    this.state.tamanos.margenSuperior= obj.margenSuperior;
+    this.state.tamanos.cuerpo = obj.cuerpo;
+   
+  }
+
+  
+
+  calcularTamanos(){
+    var width = Dimensions.get('window').width;
+    var height = Dimensions.get('window').height;
+
+    var retorno = {
+
+      margenSuperior:0,
+      cuerpo:0
+
+    };
+
+    retorno.margenSuperior = 4*height;
+    retorno.margenSuperior = retorno.margenSuperior/100;
+    retorno.margenSuperior=Math.trunc(retorno.margenSuperior);
+
+
+    retorno.cuerpo = height-retorno.margenSuperior;
+    
+
+    return retorno;
+
+
   }
 
   validarCorreo(){
@@ -94,87 +141,92 @@ export default class Ini extends Component{
   }
 	
 	render(){
+
+      console.log(this.state.tamanos);
+     
       const image = { uri: "https://static.vecteezy.com/system/resources/previews/000/266/873/non_2x/sky-background-with-clouds-layers-vector.jpg" };
 			return(
 				<View style={{width:'100%',height:'100%'}}>
-					<View style={{width:'100%',height:'4%', backgroundColor:'#053A88'}}>
+					<View style={{width:'100%',height:this.state.tamanos.margenSuperior, backgroundColor:'#053A88'}}>
 						
 					</View>
 					
-					<View style={{width:'100%',height:'96%',backgroundColor:'#1159BF'}}>
-              <ImageBackground source={image} style={{width:'100%',height:'100%'}}>
-						
-                <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
-                  <Text style={{fontSize:35, color:'white'}}>Registrarse</Text>
+            <ScrollView>
+  			       <View style={{width:'100%',height:600,backgroundColor:'#1159BF'}}>
+                <ImageBackground source={image} style={{width:'100%',height:'100%'}}>
+  						
+                  <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
+                    <Text style={{fontSize:35, color:'white'}}>Registrarse</Text>
+                    
+                  </View>
+                  <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
                   
-                </View>
-                <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
+                    <TextInput 
+                      style ={estiloInput}
+                      
+                      placeholder ="correo"
+                      onChangeText={(data)=>{
+
+                        this.state.correo.texto=data;
+                        this.forceUpdate();
+
+                        this.validarCorreo();
+
+
+                      }}
+                    />
+                    <TextInput 
+                      style ={estiloInput}
+                      secureTextEntry={true}
+                      placeholder ="Contraceña"
+                      onChangeText={(data)=>{
+
+                        this.state.contracena.uno=data;
+                        this.forceUpdate();
+
+                        this.compararContracenas();
+
+
+                      }}
+                    />
+
+                    <TextInput 
+                      style ={estiloInput}
+                      secureTextEntry={true}
+                      placeholder ="confirmar contracena"
+                      onChangeText={(data)=>{
+
+                        this.state.contracena.dos=data;
+                        this.forceUpdate();
+
+                        this.compararContracenas();
+
+
+                      }}
+                    />
+                    
+                    <Button 
+                      
+                      style={{marginBottom:10}}
+                      title="Guardar"
+                      onPress={()=>{
+                        this.props.callback('configuracion',null);
+                      }}
+                      
+                    
+                    />
+                    
+                    
+                    
+                  </View>
                 
-                  <TextInput 
-                    style ={estiloInput}
+                  <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
+                    <Text style={{fontSize:20, color:'#053A88'}}>Nad Aplication</Text>
                     
-                    placeholder ="correo"
-                    onChangeText={(data)=>{
-
-                      this.state.correo.texto=data;
-                      this.forceUpdate();
-
-                      this.validarCorreo();
-
-
-                    }}
-                  />
-                  <TextInput 
-                    style ={estiloInput}
-                    secureTextEntry={true}
-                    placeholder ="Contraceña"
-                    onChangeText={(data)=>{
-
-                      this.state.contracena.uno=data;
-                      this.forceUpdate();
-
-                      this.compararContracenas();
-
-
-                    }}
-                  />
-
-                  <TextInput 
-                    style ={estiloInput}
-                    secureTextEntry={true}
-                    placeholder ="confirmar contracena"
-                    onChangeText={(data)=>{
-
-                      this.state.contracena.dos=data;
-                      this.forceUpdate();
-
-                      this.compararContracenas();
-
-
-                    }}
-                  />
-                  
-                  <Button 
-                    
-                    style={{marginBottom:10}}
-                    title="Guardar"
-                    onPress={()=>{
-                      this.props.callback('configuracion',null);
-                    }}
-                    
-                  
-                  />
-                  
-                  
-                  
-                </View>
-              
-                <View style={{width:'100%',height:'32%',display:'flex', justifyContent :'center', alignItems:'center'}}>
-                  <Text style={{fontSize:20, color:'#053A88'}}>Nad Aplication</Text>
-                  
-                </View>
-              </ImageBackground>
-					</View>
+                  </View>
+                </ImageBackground>
+  					</View>
+          </ScrollView>
 				</View>
 			
 			);
@@ -199,3 +251,4 @@ var estiloInput ={
 	padding:10
 	
 }
+
