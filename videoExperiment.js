b24a632f6d5727f0d2ad9511a89dbd2c9c42dbaa
Nad GarDez <iranad@hotmail.com>
2020-07-08 19:45:30 -0400

    iniciando la construccion de video stream

--- videoExperiment.js
+++ videoExperiment.js
@@ -1,6 +1,10 @@
 import React, { Component,useRef } from 'react'; 
 import {StyleSheet,AppRegistry, ScrollView, Image, Text, View, TouchableOpacity,Button,Alert,TextInput,ImageBackground} from 'react-native';
 import { Camera } from 'expo-camera';
+import { Audio } from 'expo-av';
+import * as Permissions from 'expo-permissions';
+import {RTCView,getUserMedia} from 'react-native-webrtc';
+//import Permissions from 'react-native-permissions';
 
 export default class Record extends Component{
 
@@ -8,148 +12,57 @@ export default class Record extends Component{
 		super(props);
 
 		this.state={
+			videoURL:null,
 			permiso:null,
-			type:null,
-			ref:null
+			permiso2:null
 
 		}
 
 		
 
-		this.permiso= this.permiso.bind(this);
-		this.photo= this.photo.bind(this);
-		this.video= this.video.bind(this);
-		this.referenciar= this.referenciar.bind(this);
+		this.iniciarStream= this.iniciarStream.bind(this);
 	}
 
-	referenciar(ref){
+	iniciarStream(){
 
-		this.state.ref= ref;
-		
-
-		this.forceUpdate();
-		console.log(this.state.ref);
+		getUserMedia({
+	        audio: true,
+	        video: {
+	          mandatory: {},
+	        }
+	    },
+	    stream => {
+	    	console.log(stream);
+	       this.setState({ videoURL: stream.toURL() });
+	    },
+	      err => console.error(err)
+	    );
 
 	}
 
 
-	async permiso(){
-
-
-		permiso = await Camera.requestPermissionsAsync();
-		
+	componentDidMount() {
+	  	permiso = await Permissions.getAsync('CAMERA');
+		permiso2 = await Permissions.getAsync('AUDIO_RECORDING');
 		this.state.permiso= permiso;
+		this.state.permiso2= permiso2;
 
-		this.state.type= Camera.Constants.Type.front;
+		
 		this.forceUpdate();
-	}
-
-	async photo(){
-
-		var referencia= this.state.ref;
-
-		let photo = await referencia.takePictureAsync();
-
-		console.log(photo);
-
-	}
-
-	async video(){
-
-		var referencia= this.state.ref;
-
-		let video = await referencia.recordAsync();
-
-		var intervalo = setIntervarl(
-			
-			async ()=>{
-
-
-				referencia.stopRecording();
-				video = await referencia.recordAsync();
-				
-			},
-
-			1000
-
-
-		);
-
-		console.log(photo);
-
-	}
-
-	componentWillMount(){
-
-		this.permiso();
-
-	}
-
-	
+  	}
 	render(){
 
 		
+		<View style={{height:'100%',width:'100%',backgroundColor:'blue',flex:1}}>
+			<TouchableOpacity
+				onPress={
 
+					this.iniciarStream
+				}
+			>
+			</TouchableOpacity>
 
-		if(this.state.permiso==null){
-
-				
-
-				return(
-
-					<View>
-						<Text>No Granted</Text>
-					</View>
-
-
-
-				);
-
-
-		}
-
-		else{
-
-			return(
-
-					<View style={{height:'100%',width:'100%',backgroundColor:'blue'}}>
-
-						<Camera type={this.state.type} style={{height:'100%',width:'100%'}} 
-							ref={
-								this.referenciar
-							}
-						>
-
-							<View style={{display:'flex',alignItems:'space-around',justifyContent :'center',height:'100%',width:'100%'}}>
-
-
-								<TouchableOpacity onPress={
-
-
-									this.photo
-
-								}
-								>
-									<Text>Tomar foto</Text>
-								</TouchableOpacity>
-								<TouchableOpacity >
-									<Text>Grabar Video</Text>
-								</TouchableOpacity>
-
-
-
-							</View>
-	        
-      					</Camera>
-						
-					</View>
-
-
-				);
-
-
-
-		}
+		</View>
 
 
     
